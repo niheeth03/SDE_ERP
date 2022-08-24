a5c1cb501f1a0debc3413712563db5e040f59d4f
prasannakumar-23 <prasannajakku23@gmail.com>
2022-08-24 15:49:54 +0530

    dcommit

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const dotenv=require("dotenv");
 const mongoose =require("mongoose");
 const registerRoute =require("./routes/register.js");
-const emailRoute=require("./routes/groupEmail.js")
 const User = require("./models/User"); 
 
 mongoose
@@ -17,8 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/register", registerRoute);
-app.use("/groupEmail", emailRoute);
 
-app.listen("5000",()=>{
+app.listen("3080",()=>{
     console.log("Backend is running");
 });
--- api/routes/register.js
+++ api/routes/register.js
@@ -1,7 +1,6 @@
 const express=require('express');
-const app=express();
 const router=require("express").Router();
-const User = require("./../models/User.js");
+// const User = require("./../models/User.js");
 // const bcrypt = require('bcrypt');
 
 router.get("/", (req, res)=>{
@@ -15,14 +14,16 @@ router.post("/",async(req,res)=>{
         // const salt=await bcrypt.genSalt(10);
         // const hashedPass=await bcrypt.hash(req.body.password,salt)
         console.log(req.body);
-        const newUser=new User(req.body);
-        console.log(newUser.name);
-        const user=await newUser.save();
+        // const newUser=new User(req.body);
+        // console.log(newUser.name);
+        // const user=await newUser.save();
         console.log("errorrrrrr");
-        res.status(200).json(user);
+        res.send(req.body);
+        // res.status(200).json(user);
     }catch(err){
         console.log(err);
-        res.status(500).json(err);
+        res.send("error");
+        // res.status(500).json(err);
     }});
 
 module.exports=router
