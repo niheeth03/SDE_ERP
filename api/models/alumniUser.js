c48b6ec593f9ddf8090371ad42d3e733fb175b49
cs20b047 <91303372+vibhav-08@users.noreply.github.com>
2022-08-25 23:02:28 +0530

    No major changes

--- api/models/alumniUser.js
+++ api/models/alumniUser.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const { db } = require("./intladmUser");
 
 
 const UserSchema= mongoose.Schema({
--- src/alumni_portal/index.js
+++ src/alumni_portal/index.js
@@ -10,8 +10,8 @@ const Alumni_portal = () => {
   return (
     <div>
       <nav>
-        <Link to="">Home </Link>
-        <Link to="payments">Payments</Link>
+        <Link to="" style={{color: 'white', margin: '2% 2%', fontSize: '125%', textDecoration: 'none', postion: 'relative', float: 'right'}}>Home </Link>
+        <Link to="payments" style={{color: 'white', margin: '2% 2%', fontSize: '125%', textDecoration: 'none', postion: 'relative', float: 'right'}}>Payments</Link>
       </nav>
       <Outlet />
     </div>
