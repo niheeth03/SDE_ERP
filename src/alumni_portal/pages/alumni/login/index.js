678c54f1f7202f864b13d9b4e6c70802862e528e
Chakradhar4447 <cs20b044@iittp.ac.in>
2022-10-03 22:01:58 +0530

    No major changes.Commiting to pull

--- src/alumni_portal/pages/alumni/login/index.js
+++ src/alumni_portal/pages/alumni/login/index.js
@@ -21,7 +21,7 @@ const Login=()=>{
      e.preventDefault();
      await axios.post("./login",Login_Data).then((res)=>{
       if(res.data.message){setError(true);console.log(loginStatus);console.log("You are not logged in");}
-      else {setLoginStatus(res.data[0].email);console.log("Calling from alum_login POST Aprroved");console.log("Correct combination2");console.log(loginStatus);navigate("../payments")}
+      else {setLoginStatus(res.data[0].email);console.log("Calling from alum_login POST Aprroved");console.log("Correct combination2");console.log(loginStatus);navigate("../home")}
      });
   }
   return (
